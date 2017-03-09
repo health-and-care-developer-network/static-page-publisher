58a9057c23bed1bcafe278984f024d51fe75c331
Adam Hatherly <adam.hatherly@nhs.net>
2017-03-09 16:44:30 +0000

    Updated to clean up old content

--- publish.sh
+++ publish.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Usage compilePages.sh registry_host target_host github_url branch
+# Usage compilePages.sh registry_host target_host github_url branch dir1 dir2 dir3 dir4 dir5 dir6
 
 REGISTRY_HOST=$1
 TARGET_HOST=$2
 GITHUB_URL=$3
 BRANCH=${4:-master}
+DIR1=$5
+DIR2=$6
+DIR3=$7
+DIR4=$8
+DIR5=$9
+DIR6=$10
+
 VOLUME_PATH=${VOLUME_PATH:-/docker-data/static-pages}
 
 IMAGE_NAME=nhsd/static-publish
@@ -38,5 +45,5 @@ docker $TARGET_PREFIX rm staticpublish
 docker $TARGET_PREFIX run \
 	--name staticpublish \
 	-v $VOLUME_PATH:/content \
-	$SOURCE_URL $GITHUB_URL $BRANCH
+	$SOURCE_URL $GITHUB_URL $BRANCH $DIR1 $DIR2 $DIR3 $DIR4 $DIR5 $DIR6
 docker $TARGET_PREFIX rm staticpublish
--- publisher/entrypoint.sh
+++ publisher/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Usage: entrypoint.sh git_url branch
+# Usage: entrypoint.sh git_url branch dir1 dir2 dir3 dir4 dir5 dir6
 GIT_URL=$1
 BRANCH=$2
+DIR1=$3
+DIR2=$4
+DIR3=$5
+DIR4=$6
+DIR5=$7
+DIR6=$8
 
 ROOT_PATH="/content"
 TEMP_PATH="/content/tmp"
@@ -19,10 +25,40 @@ git checkout $BRANCH
 
 # Delete the old content and move the new content in its place
 cd $ROOT_PATH
-mkdir old
-mv * old
-mv old/tmp/* .
 
-# And clean up the temp files
-rm -Rf old
+if [ ! -z $DIR1 ]
+then
+  rm -Rf $DIR1; mv tmp/$DIR1 .
+fi
+if [ ! -z $DIR2 ]
+then
+  rm -Rf $DIR2; mv tmp/$DIR2 .
+fi
+if [ ! -z $DIR3 ]
+then
+  rm -Rf $DIR3; mv tmp/$DIR3 .
+fi
+if [ ! -z $DIR4 ]
+then
+  rm -Rf $DIR4; mv tmp/$DIR4 .
+fi
+if [ ! -z $DIR5 ]
+then
+  rm -Rf $DIR5; mv tmp/$DIR5 .
+fi
+if [ ! -z $DIR6 ]
+then
+  rm -Rf $DIR6; mv tmp/$DIR6 .
+fi
+if [ ! -z $DIR7 ]
+then
+  rm -Rf $DIR7; mv tmp/$DIR7 .
+fi
+if [ ! -z $DIR8 ]
+then
+  rm -Rf $DIR8; mv tmp/$DIR8 .
+fi
+
+# And clean up any remaining temp files
+rm -Rf tmp
 
